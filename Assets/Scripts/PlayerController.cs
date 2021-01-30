3625542efce4046cd315b2c1b73c6ed8cb26f666
Nick Garza <adagar121987@gmail.com>
2021-01-29 23:45:12 -0500

    Added palyer rotation, DOF to scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.25f;
+    public GameObject playerModel;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,9 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(speed * horizontalInput, 0, speed * verticalInput);
 
-
+        playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, 
+                                                          Quaternion.LookRotation(
+                                                          new Vector3(speed * verticalInput, 0, -speed * horizontalInput)), 
+                                                          Time.deltaTime * 30f);
     }
 }
