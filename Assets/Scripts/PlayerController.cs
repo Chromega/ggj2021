5fa72340d2a2114cbf5304c425b08b08c8bfdbf8
Mark Sullivan <markasullivaniii@gmail.com>
2021-01-30 20:03:47 -0500

    whoops checked in bad merge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -51,17 +51,16 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("moving", true);
 
             float xzSpeed = (new Vector2(speedToUse * horizontalInput, speedToUse * verticalInput)).magnitude;
-            animator.SetFloat("walk_speed", xzSpeed*.75f);
+            animator.SetFloat("walk_speed", xzSpeed * .75f);
             playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation,
                                                               Quaternion.LookRotation(
                                                               new Vector3(speed * verticalInput, 0, -speed * horizontalInput)),
                                                               Time.deltaTime * 30f);
         }
-<<<<<<< HEAD
         else
         {
             animator.SetBool("moving", false);
-=======
+        }
 
         if (Input.GetKey("space"))
         {
@@ -70,7 +69,6 @@ public class PlayerController : MonoBehaviour
         else if(myInteractor.interactionInProgress)
         {
             myInteractor.CancelInteraction();
->>>>>>> 241f87f3cdbec2313498a17c6305e8a6ce8c655d
         }
     }
 }
