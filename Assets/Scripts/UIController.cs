7674eabafeb3632e12e86c8a337c96a5bdaf193a
spicy-happy <xin+git@xin-squared.com>
2021-01-31 06:31:39 -0800

    credits

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour {
 
 	public CanvasGroup Title;
+	public CanvasGroup Credits;
 
 	public void Update()
 	{
 		StartCoroutine(FadeTitle(false,.003f));
+
+		// StartCoroutine(FadeCredits(true,.002f));
 	}
 
 	public IEnumerator FadeTitle(bool fadeToBlack = false, float fadeSpeed = .3f)
@@ -29,6 +32,26 @@ public class UIController : MonoBehaviour {
 			while (Title.alpha > 0)
 			{
 				Title.alpha -= fadeSpeed * Time.deltaTime;
+				yield return null;
+			}
+		}
+	}
+
+	public IEnumerator FadeCredits(bool fadeToBlack = false, float fadeSpeed = .3f)
+	{
+
+		if (fadeToBlack)
+		{
+			while (Credits.alpha < 1)
+			{
+				Credits.alpha += fadeSpeed * Time.deltaTime;
+				yield return null;
+			}
+		} else
+		{
+			while (Credits.alpha > 0)
+			{
+				Credits.alpha -= fadeSpeed * Time.deltaTime;
 				yield return null;
 			}
 		}
