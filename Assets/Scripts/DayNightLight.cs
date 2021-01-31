6166ea2f80f331c83f0c3f6b64f076937b1187d0
Sean Liu (AR/VR) <sean.y.liu@gmail.com>
2021-01-30 17:51:11 -0800

    move the shadow with day and night

--- Assets/Scripts/DayNightLight.cs
+++ Assets/Scripts/DayNightLight.cs
@@ -8,6 +8,8 @@ public class DayNightLight : MonoBehaviour
     public float dayIntervalSec = 60f;
     public int tempDay = 6500;
     public int tempNight = 20000;
+    public float yDay = -90.0f;
+    public float yNight = 90.0f;
 
     private float sinPeriod;
 
@@ -23,5 +25,9 @@ public class DayNightLight : MonoBehaviour
         float amplitude = (Mathf.Cos(sinPeriod * Time.time) + 1.0f) / 2.0f; // clamp 0 -> 1
         float temp = (tempNight - tempDay) * 1.0f * amplitude + tempDay;
         sun.colorTemperature = temp;
+
+        float yRotation = (yNight - yDay) * 1.0f * amplitude + yDay;
+        sun.transform.rotation = Quaternion.Euler(sun.transform.rotation.eulerAngles.x, yRotation, sun.transform.rotation.eulerAngles.z);
+        // new Vector3(sun.transform.rotation.x, yRotation, sun.transform.rotation.z);
     }
 }
