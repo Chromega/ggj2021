cbfc742e28db0aa6da4f9d1db5668e1f41048059
spicy-happy <xin+git@xin-squared.com>
2021-01-31 09:43:35 -0800

    partial lighting for mark

--- Assets/Scripts/DayNightLight.cs
+++ Assets/Scripts/DayNightLight.cs
@@ -10,6 +10,8 @@ public class DayNightLight : MonoBehaviour
     public int tempNight = 20000;
     public float yDay = -90.0f;
     public float yNight = 90.0f;
+    public int intensityDay = 120000;
+    public int intensityNight = 1000;
 
     private float sinPeriod;
 
@@ -25,6 +27,10 @@ public class DayNightLight : MonoBehaviour
         float amplitude = (Mathf.Cos(sinPeriod * Time.time) + 1.0f) / 2.0f; // clamp 0 -> 1
         float temp = (tempNight - tempDay) * 1.0f * amplitude + tempDay;
         sun.colorTemperature = temp;
+
+        float intensity = (intensityNight - intensityDay) * 1.0f * amplitude + intensityDay;
+        sun.intensity = intensity;
+        Debug.Log(intensity);
 
         float yRotation = (yNight - yDay) * 1.0f * amplitude + yDay;
         sun.transform.rotation = Quaternion.Euler(sun.transform.rotation.eulerAngles.x, yRotation, sun.transform.rotation.eulerAngles.z);
