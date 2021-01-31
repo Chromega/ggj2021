9ea6c92d6db0303fd775e85f4484f5122721bb00
spicy-happy <xin+git@xin-squared.com>
2021-01-31 07:48:42 -0800

    instructions

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,12 +7,60 @@ public class UIController : MonoBehaviour {
 
 	public CanvasGroup Title;
 	public CanvasGroup Credits;
+	public CanvasGroup Instructions;
+
+	int keyNum = 0;
+	bool mouseWheel = false;
+	bool space = false;
+	bool shift = false;
 
 	public void Update()
 	{
-		StartCoroutine(FadeTitle(false,.003f));
 
+		StartCoroutine(FadeTitle(false,.003f));
+		StartCoroutine(FadeInstructions(true,.05f));
+
+
+		// Call this when you want credits to show
 		// StartCoroutine(FadeCredits(true,.002f));
+
+		if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow)) {
+			keyNum ++;
+		}
+
+		if (keyNum == 3) {
+			RawImage image = Instructions.transform.Find("01").GetComponent<RawImage>();
+			image.color = new Color(1,1,1,0);
+
+			Instructions.transform.Find("02").gameObject.SetActive(true);
+		}
+
+		if (Input.GetAxis("Mouse ScrollWheel") != 0 && keyNum > 2) {
+			RawImage image = Instructions.transform.Find("02").GetComponent<RawImage>();
+			image.color = new Color(1,1,1,0);
+			Instructions.transform.Find("03").gameObject.SetActive(true);
+			mouseWheel = true;
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space) && mouseWheel) {
+
+			RawImage image = Instructions.transform.Find("03").GetComponent<RawImage>();
+			image.color = new Color(1,1,1,0);
+			Instructions.transform.Find("04").gameObject.SetActive(true);
+			space = true;
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift) && space) {
+			RawImage image = Instructions.transform.Find("04").GetComponent<RawImage>();
+			image.color = new Color(1,1,1,0);
+			Instructions.transform.Find("05").gameObject.SetActive(true);
+			shift = true;
+		}
+
+		if (Input.GetKeyUp(KeyCode.F) && shift) {
+			RawImage image = Instructions.transform.Find("05").GetComponent<RawImage>();
+			image.color = new Color(1,1,1,0);
+		}
 	}
 
 	public IEnumerator FadeTitle(bool fadeToBlack = false, float fadeSpeed = .3f)
@@ -52,6 +100,28 @@ public class UIController : MonoBehaviour {
 			while (Credits.alpha > 0)
 			{
 				Credits.alpha -= fadeSpeed * Time.deltaTime;
+				yield return null;
+			}
+		}
+	}
+
+	public IEnumerator FadeInstructions(bool fadeToBlack = false, float fadeSpeed = .3f)
+	{
+
+		yield return new WaitForSeconds(7);
+
+		if (fadeToBlack)
+		{
+			while (Instructions.alpha < 1)
+			{
+				Instructions.alpha += fadeSpeed * Time.deltaTime;
+				yield return null;
+			}
+		} else
+		{
+			while (Instructions.alpha > 0)
+			{
+				Instructions.alpha -= fadeSpeed * Time.deltaTime;
 				yield return null;
 			}
 		}
